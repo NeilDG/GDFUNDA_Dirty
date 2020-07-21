0c7378ee0dd95019983f4c65c85e56503fe49006
clawy <raina_cally_ang@dlsu.edu.ph>
2020-07-21 15:31:43 +0800

    added raina debug log

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -12,6 +12,7 @@ public class TestScript : MonoBehaviour
         this.Print_Marielle();
         this.Print_Lo();
         this.Print_Gian();
+        this.Print_Raina();
     }
 
     // Update is called once per frame
@@ -42,5 +43,10 @@ public class TestScript : MonoBehaviour
     private void Print_Lo() 
     {
         Debug.Log("I am Jacob");
+    }
+
+    private void Print_Raina()
+    {
+        Debug.Log("This is Raina :^)");
     }
 }
