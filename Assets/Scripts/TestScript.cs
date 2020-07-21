d2642ec561a7c7cd0b585d2d29b6e2b7f07911b2
NeilDG <fm.atropos@gmail.com>
2020-07-21 17:12:15 +0800

    Updated dev branch.

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -13,10 +13,7 @@ public class TestScript : MonoBehaviour
         this.Print_Lo();
         this.Print_Gian();
         this.Print_Manzano();
-<<<<<<< HEAD
-=======
         this.Print_Paolo();
->>>>>>> 4f51226a8ad2d503d914b6829c338a86ec6d8d3a
         this.Print_Raina();
         this.Print_Lanz();
         this.Print_Nathan();
@@ -57,11 +54,7 @@ public class TestScript : MonoBehaviour
     {
         Debug.Log("I am the Jacob from the Manila Campus");
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 4f51226a8ad2d503d914b6829c338a86ec6d8d3a
     private void Print_Raina()
     {
         Debug.Log("This is Raina :^)");
