f33e0af7dca98de7acf3df7ff2b7e9527f885472
Clickery <garret_villanueva@dlsu.edu.ph>
2020-07-22 21:04:40 +0800

    Merge branch 'master' of https://github.com/NeilDG/GDFUNDA_Dirty into garret
    # Conflicts:
    #	Assets/Scenes/MainScene.unity

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -12,6 +12,13 @@ public class TestScript : MonoBehaviour
         this.Print_Marielle();
         this.Print_Lo();
         this.Print_Gian();
+        this.Print_Manzano();
+        this.Print_Paolo();
+        this.Print_Raina();
+        this.Print_Lanz();
+        this.Print_Nathan();
+        this.Print_Richmond();
+        this.Print_Warren();
     }
 
     // Update is called once per frame
@@ -27,14 +34,13 @@ public class TestScript : MonoBehaviour
 
     private void Print_Neil()
     {
-        Debug.Log("I am Neil.");
+        Debug.Log("I am Neil Del Gallego! Wahahaha.");
     }
 
     private void Print_Gian(){
         Debug.Log("Hewwo OwO I am Gian UwU");
     }
     
-
     private void Print_Marielle()
     {
         Debug.Log("I am Marielle.");
@@ -42,5 +48,41 @@ public class TestScript : MonoBehaviour
     private void Print_Lo() 
     {
         Debug.Log("I am Jacob");
+    }
+
+    private void Print_Manzano()
+    {
+        Debug.Log("I am the Jacob from the Manila Campus");
+    }
+
+    private void Print_Raina()
+    {
+        Debug.Log("This is Raina :^)");
+    }
+
+    private void Print_Paolo()
+    {
+        Debug.Log("PAOUWU");    
+    }
+    
+    private void Print_Lanz()
+    {
+        string name = "Lanz";
+        Debug.Log($"<b>Howdy!</b> This is <color=red>{name}</color>");
+    }
+
+    private void Print_Nathan()
+    {
+        Debug.Log("I am Nathan");
+    }
+
+    private void Print_Richmond()
+    {
+        Debug.Log("I Am Richmond");
+    }
+    
+    private void Print_Warren()
+    {
+        Debug.Log("Woah, it is Warren");
     }
 }
